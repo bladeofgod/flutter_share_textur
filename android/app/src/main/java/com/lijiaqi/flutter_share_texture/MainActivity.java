d47c7821817c457222ecd1fe38abb389a7c78eb4
bladeofgod <450103187@qq.com>
2021-02-16 09:26:45 +0800

    1.0

---
+++ android/app/src/main/java/com/lijiaqi/flutter_share_texture/MainActivity.java
@@ -0,0 +1,6 @@
+package com.lijiaqi.flutter_share_texture;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/demo/first_step_page.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+class FirstStepPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return FirstStepPageState();
+  }
+
+}
+
+class FirstStepPageState extends State<FirstStepPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+    );
+  }
+}
